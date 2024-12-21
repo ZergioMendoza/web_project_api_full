3d036074296dc23a61f6c925c27487ea1e788403
Sergio Mendoza Méndez <zergiomendoza96@hotmail.com>
2024-12-21 11:52:32 -0500

    ajustes

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,6 +1,6 @@
 
 //const BASE_URL = 'http://localhost:3001/api';
- const BASE_URL = 'https://api.instabook.mooo.com';
+ const BASE_URL = 'https://api.instabook.mooo.com/api';
 
 
 // Registrar usuario
